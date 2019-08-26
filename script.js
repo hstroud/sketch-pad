3d61d748f8c2c04d199d62b5ae8b3a013a3a4ea1
Harley <stroudh24@gmail.com>
2019-08-26 15:19:19 -0400

    Users can now create new grids with custom dimensions.

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const sketchBox = document.getElementById("main-sketch-box");
+let numOfSquares = 16;
 
+function createSquares(numOfSquare) {
+    
+    let squareSize = 700 / numOfSquare;
+    let squareSizeString = squareSize.toString() + "px"
+    numOfSquare *= numOfSquare;
 
-for (let i = 0; i < 256; i++) {
-    let drawSquare = document.createElement("div");
-    drawSquare.classList.add("drawingSquare");
-
-    drawSquare.addEventListener("mouseover", function() {
-        drawSquare.style.backgroundColor = "black"
-    });
-
-    sketchBox.append(drawSquare);
+    for (let i = 0; i < numOfSquare; i++) {
+        let drawSquare = document.createElement("div");
+        drawSquare.classList.add("drawingSquare");
+        drawSquare.style.height = squareSizeString;
+        drawSquare.style.width = squareSizeString;
+    
+        drawSquare.addEventListener("mouseover", function() {
+            drawSquare.style.backgroundColor = "black"
+        });
+    
+        sketchBox.append(drawSquare);
+    }
 }
+
+createSquares(numOfSquares);
+
 
 function createNewGrid() {
     sketchDivs = document.querySelectorAll(".drawingSquare");
-
     sketchDivs.forEach((sketchDiv) => {
-        sketchDiv.style.backgroundColor = "white"
+        sketchDiv.remove();
     });
+
+    numOfSquares = prompt("Enter the dimensions for the new grid (ex: enter 64 to produce a grid that is 64x64");
+    createSquares(numOfSquares);
+
 }
 
 
